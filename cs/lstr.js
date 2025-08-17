419d6ce17047213f666f4ab4242023319946ffbb
VSimon <vita.simon@centrum.cz>
2025-08-17 12:40:30 +0200

    CZ,EN : pLocalizationSwitcher : dynamic string feature added connected changes (new input parameter)

--- cs/lstr.js
+++ cs/lstr.js
@@ -1,4 +1,4 @@
 var _lstr = {
-  'MSG_NODATA' : () => `Nebyl určen soubor nápovědy - zadejte parametr <b>?${PAR_NAME_DOC}=</b> do adresního řádku. Byl použit výchozí název <b>${dataPath}</b>. Pokud je panel prohlížení prázdný, pak soubor <b>${dataPath}</b> pravděpodobně neexistuje v místním adresáři prohlížeče. V dalším kroku zkuste zkontrolovat parametr <b>${PAR_NAME_PAGE}=</b>, abyste ověřili, zda ukazuje na soubor, který se v souboru nápovědy skutečně nachází.`,
-  'MSG_PATH_NOT_FOUND_IN_ARCH' : () => `Zadaná cesta <b>${pagePath}</b> se v souboru nápovědy <b>${dataPath}</b> nenachází.`,
+  'MSG_NODATA' : (s) => `Nebyl určen soubor nápovědy - zadejte parametr <b>?${PAR_NAME_DOC}=</b> do adresního řádku. Byl použit výchozí název <b>${dataPath}</b>. Pokud je panel prohlížení prázdný, pak soubor <b>${dataPath}</b> pravděpodobně neexistuje v místním adresáři prohlížeče. V dalším kroku zkuste zkontrolovat parametr <b>${PAR_NAME_PAGE}=</b>, abyste ověřili, zda ukazuje na soubor, který se v souboru nápovědy skutečně nachází.`,
+  'MSG_PATH_NOT_FOUND_IN_ARCH' : (s) => `Zadaná cesta <b>${pagePath}</b> se v souboru nápovědy <b>${dataPath}</b> nenachází.`,
 };
